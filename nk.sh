550f6d122993fa1d7ed8ced6fc664487e57c45af
navin136 <nkwhitehat@gmail.com>
2022-10-15 21:39:34 +0530

    test

--- nk.sh
+++ nk.sh
@@ -1,2 +1,8 @@
-ping google.com & sleep 5s
-ping google.com & sleep 5s
+df -h
+free -h
+sudo fallocate -l 16G /nk/swapfile
+sudo chmod 600 /nk/swapfile
+sudo mkswap /nk/swapfile
+sudo swapon /nk/swapfile
+sudo swapon
+free -h
