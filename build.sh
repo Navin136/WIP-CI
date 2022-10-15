20f344c97cfa56e349af18a2c6d708705c193aff
navin136 <nkwhitehat@gmail.com>
2022-10-15 06:42:36 +0530

    Again OOM oof

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync --fail-fast -j8 # to sync failed repo if .......
 . build/envsetup.sh
 lunch arrow_X00T-eng
-mka bacon -j10 |& tee build.log & sleep 95m
+mka bacon -j$(nproc --all) |& tee build.log & sleep 90m
